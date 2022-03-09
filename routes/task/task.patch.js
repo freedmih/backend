2368df8d21ec8434806e5724481e020eb60f9e3d
freedmih <kozlenkomichael@gmail.com>
2022-03-09 12:38:45 +0300

    Add validation to PATCH router

--- routes/task/task.patch.js
+++ routes/task/task.patch.js
@@ -1,16 +1,30 @@
 const { patchTask } = require("../../db/memory");
+const { body, param, validationResult } = require('express-validator');
 var express = require('express');
 var router = express.Router();
 
-router.patch('/:uuid', async function (req, res) {
+router.patch(
+    '/:uuid',
 
-    const { uuid } = req.params;
+    param('uuid').isUUID(4),
+    body('name').optional().isLength({ max: 20 }),
+    body('done').optional().isBoolean(),
+    body('createdAt').optional().isDate(),
 
-    const task = req.body;
+    async function (req, res) {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
 
-    const result = await patchTask({ uuid, ...task });
+        const { uuid } = req.params;
 
-    return res.status(result.code).send(result.message);
-});
+        const task = req.body;
+
+        const result = await patchTask({ uuid, ...task });
+
+        return res.status(result.code).send(result.message);
+    }
+);
 
 module.exports = router;
