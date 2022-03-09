f640f057fa5141b62e64b0bff0a1e171815f2fde
freedmih <kozlenkomichael@gmail.com>
2022-03-09 12:33:07 +0300

    Add validation to DELETE router

--- routes/task/task.delete.js
+++ routes/task/task.delete.js
@@ -1,14 +1,25 @@
-const {removeTask} = require("../../db/memory");
+const { removeTask } = require("../../db/memory");
+const { param, body, validationResult } = require('express-validator');
 var express = require('express');
 var router = express.Router();
 
-router.delete('/:uuid', async function (req, res) {
+router.delete(
+    '/:uuid',
 
-    const { uuid } = req.params;
+    param('uuid').isUUID(4),
 
-    const removeResult = await removeTask(uuid);
+    async function (req, res) {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
 
-    return res.status(removeResult.code).send(removeResult.message);
-});
+        const { uuid } = req.params;
+
+        const removeResult = await removeTask(uuid);
+
+        return res.status(removeResult.code).send(removeResult.message);
+    }
+);
 
 module.exports = router;
