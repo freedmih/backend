0a50b531e3cb96f7abbd32e5be61b5f21dee9a48
freedmih <kozlenkomichael@gmail.com>
2022-03-09 14:17:28 +0300

    Fix GET router validation

--- routes/task/tasks.get.js
+++ routes/task/tasks.get.js
@@ -6,10 +6,10 @@ var router = express.Router();
 router.get(
     '/',
 
-    query('filterBy').default('all').isIn(['done', 'undone']),
+    query('filterBy').default('all').isIn(['done', 'undone', 'all']),
     query('order').default('asc').isIn(['asc', 'desc']),
-    query('page').default(1).isInt(),
-    query('pp').default(1).isInt(),
+    query('page').default(0).isInt(),
+    query('pp').default(5).isInt(),
 
     async function (req, res) {
 
@@ -19,10 +19,10 @@ router.get(
             return res.status(400).json({ errors: errors.array() });
         }
 
-        const { filterBy, order, page, pp } = req.params;
-        const tasks = await getTasks({
-            filterBy, order, page, pp
-        });
+        const { filterBy, order, page, pp } = req.query;
+        const tasks = await getTasks(
+            { filterBy, order, page, pp } 
+        );
 
         return res.json(tasks);
     }
