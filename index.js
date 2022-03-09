c4fab8fe4d514bb8f24234c4f1b347456df9e727
freedmih <kozlenkomichael@gmail.com>
2022-03-09 18:09:00 +0300

    Connect to postgres db

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
+require('dotenv').config()
+
 const express = require('express');
 const app = express();
+const db = require("./db");
+
 const port = 3000;
 
 const recursive = require('recursive-readdir-sync');
+
+(async () => {
+    try {
+        await db.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+})();
 
 app.use(express.json());
 
@@ -10,7 +23,7 @@ recursive(`${__dirname}/routes`)
     .forEach(file => app.use('/', require(file)));
 
 function logErrors(err, req, res, next) {
-    return res.status(err.httpCode ?? 422).json({errors: [{msg: err.message}]});
+    return res.status(err.httpCode ?? 422).json({ errors: [{ msg: err.message }] });
 }
 
 app.use(logErrors);
