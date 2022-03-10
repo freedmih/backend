9d9f7986103705b4706289054ab64a9b92ded12e
freedmih <kozlenkomichael@gmail.com>
2022-03-10 11:09:37 +0300

    Import process for env

--- config/config.js
+++ config/config.js
@@ -5,12 +5,12 @@ module.exports =
         "username": process.env.DEV_POSTGRES_USER,
         "password": process.env.DEV_POSTGRES_PASS,
         "database": process.env.DEV_POSTGRES_DB,
-        "host": "127.0.0.1",
+        "host": process.env.DEV_POSTGRES_HOST,
         "dialect": "postgres"
     },
     "production": {
-        "database": "sequelize_todo_production",
-        "host": "127.0.0.1",
+        "database": process.env.PROD_POSTGRESS_DB,
+        "host": process.env.PROD_POSTGRESS_HOST,
         "dialect": "postgres"
     }
 }
--- db.js
+++ db.js
@@ -1,11 +1,12 @@
 const { Sequelize, DataTypes } = require('sequelize');
 
+
 module.exports = new Sequelize(
     process.env.DEV_POSTGRES_DB,
     process.env.DEV_POSTGRES_USER,
     process.env.DEV_POSTGRES_PASS,
     {
-        host: 'localhost',
+        host: process.env.DEV_POSTGRES_HOST,
         dialect: 'postgres'
     }
 );
