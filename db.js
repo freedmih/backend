c142e6d87fbc402ebcd02de7fa33b34b41173342
freedmih <kozlenkomichael@gmail.com>
2022-03-10 11:18:39 +0300

    Heroku config

--- db.js
+++ db.js
@@ -1,7 +1,14 @@
+require("dotenv").config();
 const { Sequelize, DataTypes } = require('sequelize');
 
 
-module.exports = new Sequelize(
+module.exports = process.env.NODE_ENV == "production" ? new Sequelize(
+    process.env.DATABASE_URL,
+    {
+        dialect: 'postgres'
+    }
+) :
+new Sequelize(
     process.env.DEV_POSTGRES_DB,
     process.env.DEV_POSTGRES_USER,
     process.env.DEV_POSTGRES_PASS,
