c8fcbc2e5d5d274497260c9e32ba87f011862b8d
freedmih <kozlenkomichael@gmail.com>
2022-03-16 21:21:31 +0300

    Add translation to jwt middleware

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -12,11 +12,11 @@ module.exports.validateAccessToken = req => {
   const authHeader = req.headers['authorization']
   const token = authHeader && authHeader.split(' ')[1];
 
-  if (token == null) throw new ApiError('This route requires a JWT token', 401);
+  if (token == null) throw new ApiError(res.__('requires_jwt'), 401);
 
   let result;
   jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
-    if (err) throw new ApiError('Invalid JWT token', 403);
+    if (err) throw new ApiError(res.__('invalid_jwt'), 403);
 
     result = user;
   });
